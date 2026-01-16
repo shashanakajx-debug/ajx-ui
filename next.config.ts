0498ccfc4a720d1039970b2ac3d295b471c196c4
Shashank Tripathi <stripathi0905@gmail.com>
2026-01-17 02:44:32 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias['@'] = path.join(process.cwd(), 'src');
+        return config;
+    },
     images: {
         remotePatterns: [
             {
@@ -8,6 +14,12 @@ const nextConfig = {
             },
         ],
         qualities: [65],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
     },
     experimental: {
         serverActions: {
